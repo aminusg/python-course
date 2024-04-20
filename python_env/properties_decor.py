f84566147028f34a9bc8cc84c9f3e99409f51cf9
ainhoa <aino20m@gmail.com>
2024-04-20 18:24:55 +0200

    classes and dunder methods commit

--- python_env/properties_decor.py
+++ python_env/properties_decor.py
@@ -32,5 +32,15 @@ Ejercicio de Codificacion
 We want to ensure that our size attribute can be retrieved, but not set. 
 Use the appropriate syntax to protect the size attribute. 
 Then, use the 'property' decorator to build a getter to return the protected data. 
-You do not need to instantiate the class.
+You do not need to instantiate the class
 """
+
+class Garage:
+  def __init__(self, size):
+    #   Protect the size attribute
+    self._size = size
+    self.cars = []
+
+
+  def open_door(self):
+    return "The door opens"
